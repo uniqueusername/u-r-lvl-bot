6308ba18eae50eb7019b1536b1e1cbdac5cbdb86
Aryan Jha <aryan@jha.io>
2017-05-07 00:05:35 -0700

    i removed the console.log
    also set default xp to 0
    also fixed reset xp function
    and added a quote

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var config;
 var userLevels = {};
 var userStats = {}; // timer, message count, message array
 
-var xpMessages = [" i think this is slavery but you have ", " u fuck u got ", " when do i get my paycheck, because it better be more than ", " i really am not getting paid for this, can i have some of that juicy ", " u could buy a car with this dank af ", " idk what youre smoking but thats a thicc ", " the snail sleeps for ", " dad said he went to the store but i havent seen him since the fall of ", " you are now allowed to identify as this many genders: ", " bush did "];
+var xpMessages = ["i think this is slavery but you have ", "u fuck u got ", "when do i get my paycheck, because it better be more than ", "i really am not getting paid for this, can i have some of that juicy ", "u could buy a car with this dank af ", "idk what youre smoking but thats a thicc ", "the snail sleeps for ", "dad said he went to the store but i havent seen him since the fall of ", "you are now allowed to identify as this many genders: ", "bush did ", "where is the cheeto nick"];
 
 // give xp per message
 bot.on('message', msg => {
@@ -27,6 +27,9 @@ bot.on('message', msg => {
   }*/
 
   // dynamically add xp based on amount of characters and message sent within a minute
+  if (userLevels[msg.author.id] == undefined) {
+    userLevels[msg.author.id] = 0;
+  }
   if (userLevelStats == undefined) {
     userLevelStats = { "timerEnabled": false, "messageArray": [] };
     userStats[msg.author.id] = userLevelStats;
@@ -34,7 +37,6 @@ bot.on('message', msg => {
   userLevelStats["messageArray"].push(msg.content); // push the message into the message array
   if (userLevelStats["timerEnabled"] === false) {
     const numCharacters = userLevelStats["messageArray"].join("").split(" ").join("").length // number of characters in message array without spaces
-    console.log(userLevelStats["messageArray"]);
     const averageCharacters = Math.floor( numCharacters / userLevelStats["messageArray"].length);
     userLevels[msg.author.id] += averageCharacters // add character count / number of messages to user total xp
     userLevelStats["timerEnabled"] = true; // 1 for timer enabled
@@ -56,18 +58,18 @@ bot.on('message', msg => {
 bot.on('message', msg => {
   var userXP = JSON.parse(fs.readFileSync('userLevels.json'))[msg.author.id]
 
-  if (msg.content === "where is the cheeto nick") {
+  if (msg.content.includes("where is the cheeto nick")) {
     msg.reply(xpMessages[getRandomInt(0, xpMessages.length)] + '``' + userXP + '`` xp, level ``' + getLevelFromXP(userXP) + '``');
   }
 });
 
 // xp resetter
 bot.on('message', msg => {
-  if (msg.content.match('reset xp of') != undefined && msg.author.id == '83807335650164736') {
+  if (msg.content.includes('reset xp of') && msg.author.id == '83807335650164736') {
     var userToReset = msg.content.split(' ')[3];
     var userLevels = JSON.parse(fs.readFileSync('userLevels.json'));
-  userLevels[userToReset.slice(2, -1)] = 0;
-  fs.writeFileSync('userLevels.json', JSON.stringify(userLevels), 'utf8');
+    userLevels[msg.mentions.users.firstKey()] = 0;
+    fs.writeFileSync('userLevels.json', JSON.stringify(userLevels), 'utf8');
   }
 })
 
